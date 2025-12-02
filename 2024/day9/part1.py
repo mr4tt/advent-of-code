22e989a6cb64a57cc8b8bee7d1ea16f173573285
mrat <78581772+mr4tt@users.noreply.github.com>
2025-12-01 23:48:08 -0800

    random crap from last year

---
+++ 2024/day9/part1.py
@@ -0,0 +1,12 @@
+with open("input.txt", "r+") as file1:
+    disk = file1.read()
+
+print(len(disk))
+
+expanded = []
+
+for n in range(len(disk)):
+    if n % 2 == 0:
+
+    elif n % 2 == 1:
+        expanded.append()
